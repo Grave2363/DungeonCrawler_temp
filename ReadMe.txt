8cb67d95206c1253b59b6e1b70729bdf89a0943e
Angelo Giovanni Nicotra <53791304+Grave2363@users.noreply.github.com>
2024-04-02 19:42:26 -0400

    fixed movement

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+Makes use of the godot levelblock by Cryszon
+https://github.com/Reun-Media/godot-levelblock
---
+++ Scenes/player.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+@onready var front_ray = $RayFront
+@onready var back_ray = $RayBack
+var tween
+var tween_running = false
+const travel_time = 0.3
+var dir = Vector3(0,0,0)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	if tween_running == true:
+		return
+	dir = global_transform.basis.z
+	if Input.is_action_pressed("ui_up") and not front_ray.is_colliding():
+		tween = create_tween()
+		tween.connect("finished", on_tween_finished)
+		tween.tween_property(self,"transform", transform.translated(dir * -2), travel_time)
+		tween_running = true
+	if Input.is_action_pressed("ui_down") and not back_ray.is_colliding():
+		tween = create_tween()
+		tween.connect("finished", on_tween_finished)
+		tween.tween_property(self,"transform", transform.translated(dir * 2), travel_time)
+		tween_running = true
+	if Input.is_action_pressed("ui_left") :
+		tween = create_tween()
+		tween.connect("finished", on_tween_finished)
+		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, PI / 2 ), travel_time)
+		tween_running = true
+	if Input.is_action_pressed("ui_right") :
+		tween = create_tween()
+		tween.connect("finished", on_tween_finished)
+		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, -PI / 2), travel_time)
+		tween_running = true
+
+func on_tween_finished():
+	tween_running = false
