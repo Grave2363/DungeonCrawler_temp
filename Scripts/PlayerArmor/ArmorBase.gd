83e11e0c039c92bd255dd088be6dc63f0eb06fd2
Angelo Giovanni Nicotra <53791304+Grave2363@users.noreply.github.com>
2024-05-08 12:37:12 -0400

    small change

--- Scripts/PlayerArmor/ArmorBase.gd
+++ Scripts/PlayerArmor/ArmorBase.gd
@@ -4,5 +4,6 @@ class_name ArmorBase
 
 @export var ArmorName : String = "What"
 @export var ArmorDis : String = "What is this?"
+@export var Cursesd : bool = false
 @export var def : int
 @export var mDef : int
