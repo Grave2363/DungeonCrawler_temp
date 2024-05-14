f72539638ee253cced3b18cca53679579c12143c
Angelo Giovanni Nicotra <53791304+Grave2363@users.noreply.github.com>
2024-05-13 20:22:02 -0400

    created temp chest event

---
+++ Scripts/Items/chest.gd
@@ -0,0 +1,17 @@
+extends Node3D
+signal Chest_Opened
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_static_body_3d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	emit_signal("Chest_Opened")
+	$AnimationPlayer.chest_open()
+
