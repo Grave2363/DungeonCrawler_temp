c0de7f63281ccae8c0a888474a0798e4903f6394
Angelo Giovanni Nicotra <53791304+Grave2363@users.noreply.github.com>
2024-06-17 12:05:26 -0400

    small changes to player side

--- Scripts/Combat/battle_screen.gd
+++ Scripts/Combat/battle_screen.gd
@@ -4,25 +4,37 @@ signal attack1(mp: int, dmg: int)
 signal nextTarget
 signal prevTarget
 signal player_turn_end
-var attack01 = Button.new()
-var atk01Cost = 0
-var skillArray = [attack01]
-var costArray = [atk01Cost]
+var players = []
+var attack01_player = Button.new()
+var atk01Cost_player = 0
+var attack01_npc01 = Button.new()
+var atk01Cost_npc01 = 0
+var skillArray_player01 = [attack01_player]
+var costArray_player01 = [atk01Cost_player]
+var skillArray_npc01 = [attack01_npc01]
+var costArray_npc01 = [atk01Cost_npc01]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	atk01Cost = $Panel/Player_base/Job/CharacterSkill.get_child(0).mpCost 
-	attack01.text = $Panel/Player_base/Job/CharacterSkill.get_child(0).name + "  MP : " + str(atk01Cost)
-	attack01.set_position(Vector2(20,20))
-	attack01.set_size(Vector2(80,20))
-	$Panel/Attacks.add_child(attack01)
-	attack01.connect("pressed", on_attack01_pressed)
+	atk01Cost_player = $Panel/Player_base/Job/CharacterSkill.get_child(0).mpCost 
+	attack01_player.text = $Panel/Player_base/Job/CharacterSkill.get_child(0).name + "  MP : " + str(atk01Cost_player)
+	attack01_player.set_position(Vector2(20,20))
+	attack01_player.set_size(Vector2(80,20))
+	$Panel/Attacks.add_child(attack01_player)
+	attack01_player.connect("pressed", on_attack01_pressed)
+	players.append($Panel/Player_base)
+	if $Panel/npc_01.visible == true:
+		players.append($Panel/npc_01)
 
 func on_attack01_pressed():
 	var atk01Pow = $Panel/Player_base/Job/CharacterSkill.get_child(0).power
 	var playerAtk = $Panel/Player_base/Job/Stats.get_atk()
 	var totalDmg = atk01Pow + playerAtk
-	emit_signal("attack1", atk01Cost,totalDmg)
+	emit_signal("attack1", atk01Cost_player,totalDmg)
+	player_turns_over()
+
+func player_turns_over():
 	emit_signal("player_turn_end")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
