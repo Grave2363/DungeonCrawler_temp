c07ed17e2bb627f5a594fe7709ae711c9b9d7e5a
Angelo Giovanni Nicotra <53791304+Grave2363@users.noreply.github.com>
2025-05-27 23:41:16 -0400

    temp work
    modified the page structure

---
+++ Scripts/town.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_church_pressed():
+	pass # Replace with function body.
+
+
+func _on_dungeon_pressed():
+	pass # Replace with function body.
+
+
+func _on_market_pressed():
+	pass # Replace with function body.
+
+
+func _on_black_market_pressed():
+	pass # Replace with function body.
+
+
+func _on_armorer_pressed():
+	pass # Replace with function body.
+
+
+func _on_wep_smith_pressed():
+	pass # Replace with function body.
+
+
+func _on_area_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_area_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_area_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_entrance_pressed():
+	pass # Replace with function body.
+
+
+func _on_cp_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_cp_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_cp_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_boss_cp_pressed():
+	pass # Replace with function body.
